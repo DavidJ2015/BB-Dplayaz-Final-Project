186de9219568fc42f6ce6f4f9de6b4a96643e06e
David Joslin <joslindr@gmail.com>
2018-03-03 17:42:30 -0800

    API Function Delivered

---
+++ API.R
@@ -0,0 +1,76 @@
+
+
+library(httr)
+library(jsonlite)
+library(dplyr)
+
+base <- "http://api.worldbank.org/v2/"
+
+
+## Catalog of Sources used by API
+# Using this function, we can find who provided data to the API we are using (This may or may not be useful)
+
+API_Sources <- function(){
+  pathing <- "sources"
+  query <- "?format=json"
+  url1 <- paste0(base, pathing, query)
+  Sources <- GET(url = url1)
+  List_of_Sources <- content(Sources, "text") %>% fromJSON()
+}
+# List that describes the second list below
+L1 <- API_Sources()[[1]]
+
+# Data frame that lists names of companies attributed to this API development and when they contributed to the API
+L2 <- API_Sources()[[2]]
+
+
+## Database about countries listed within this API
+
+Countries <- function(){
+  pathing <- "countries"
+  query <- paste0("?per_page=304&format=json")
+  url2 <- paste0(base, pathing, query)
+  Country <- GET(url = url2)
+  Country_List <- content(Country, "text") %>% fromJSON()
+}
+#List that describes L4 below this list
+L3 <- Countries()[[1]]
+
+# This brings about a list of 304 countries, and so this should be very, very useful
+L4 <- Countries()[[2]]
+
+
+## This is not a very useful source of information.  We probably dont want to use it.
+IncomeLevels <- function(){
+  pathing <- "incomeLevels"
+  query <- "?format=json"
+  url3 <- paste0(base, pathing, query)
+  Income <- GET(url = url3)
+  Income_List <- content(Income, "text") %>% fromJSON()
+}
+# List that describes L6 below
+L5 <- IncomeLevels()[[1]]
+
+# This is a data frame that describes income levels for only 7 different countries.  This is not very useful
+L6 <- IncomeLevels()[[2]]
+
+
+Indicators <- function(){
+  pathing <- "indicators"
+  query <- paste0("?per_page=17066&format=json")
+  url4 <- paste0(base, pathing, query)
+  Indicate <- GET(url = url4)
+  Indicator_List <- content(Indicate, "text") %>% fromJSON()
+}
+
+# List that describes L8 below
+#L7 <- Indicators()[[1]]
+
+# This is a data frame that has a list of different "indicators" that can be used to search the API site for different statistics/
+# data concerning either all the countries or certain countries during or over a time period.  This is ALOT, as it has a total 
+# of 17066 rows (please don't push a list like this to gitHub:  it wont work)  I have deselected both L7 and L8 below so that your 
+# doesn't run this function twice.  You shouldn't need your computer to write down this data frame.
+#L8 <- Indicators()[[2]]
+
+
+
