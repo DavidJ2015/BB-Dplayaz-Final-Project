168efa457c385a750428e464fa3fe7a26cde0025
minh2804 <minh2804@uw.edu>
2018-03-04 00:35:05 -0800

    Add more Tom

--- API.R
+++ API.R
@@ -1,15 +1,12 @@
-
-
-library(httr)
-library(jsonlite)
-library(dplyr)
+library("httr")
+library("jsonlite")
+library("dplyr")
 
 base <- "http://api.worldbank.org/v2/"
 
 
 ## Catalog of Sources used by API
 # Using this function, we can find who provided data to the API we are using (This may or may not be useful)
-
 API_Sources <- function(){
   pathing <- "sources"
   query <- "?format=json"
@@ -17,6 +14,7 @@ API_Sources <- function(){
   Sources <- GET(url = url1)
   List_of_Sources <- content(Sources, "text") %>% fromJSON()
 }
+
 # List that describes the second list below
 L1 <- API_Sources()[[1]]
 
@@ -25,7 +23,6 @@ L2 <- API_Sources()[[2]]
 
 
 ## Database about countries listed within this API
-
 Countries <- function(){
   pathing <- "countries"
   query <- paste0("?per_page=304&format=json")
@@ -33,12 +30,12 @@ Countries <- function(){
   Country <- GET(url = url2)
   Country_List <- content(Country, "text") %>% fromJSON()
 }
+
 #List that describes L4 below this list
 L3 <- Countries()[[1]]
 
 # This brings about a list of 304 countries, and so this should be very, very useful
 L4 <- Countries()[[2]]
-
 
 ## This is not a very useful source of information.  We probably dont want to use it.
 IncomeLevels <- function(){
@@ -48,12 +45,12 @@ IncomeLevels <- function(){
   Income <- GET(url = url3)
   Income_List <- content(Income, "text") %>% fromJSON()
 }
+
 # List that describes L6 below
 L5 <- IncomeLevels()[[1]]
 
 # This is a data frame that describes income levels for only 7 different countries.  This is not very useful
 L6 <- IncomeLevels()[[2]]
-
 
 Indicators <- function(){
   pathing <- "indicators"
--- ui.R
+++ ui.R
@@ -1,26 +1,7 @@
-shinyUI(
-  fluidPage(
-    titlePanel("Title"),
-    sidebarLayout(
-      sidebarPanel(
-        textOutput("text")
-      ),
-      mainPanel(
-        tabsetPanel(
-          tabPanel("Tab 1",
-                   textOutput("text1"),
-                   plotOutput("tab1", height = "720")), 
-          tabPanel("Tab 2",
-                   textOutput("text2"),
-                   plotOutput("tab2", height = "720")), 
-          tabPanel("Tab 3",
-                   textOutput("text3"),
-                   plotOutput("tab3", height = "720")),
-          tabPanel("Tab 4",
-                   textOutput("text4"),
-                   plotOutput("tab4", height = "720"))
-        )
-      )
-    )
-  )
+library(shinydashboard)
+
+dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody()
 )
