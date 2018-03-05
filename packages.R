810d0eb28d1fc9a94565fdb57cff7b9d1bb09150
minh2804 <minh2804@uw.edu>
2018-03-05 06:38:10 -0800

    Add plot to shiny Tom

---
+++ packages.R
@@ -0,0 +1,25 @@
+is.installed <- function(package){
+    is.element(package, installed.packages()[,1])
+}
+
+if (!is.installed("shinydashboard")){
+  install.packages("shinydashboard")
+}
+
+if (!is.installed("dplyr")){
+  install.packages("dplyr")
+}
+
+if (!is.installed("ggplot2")){
+  install.packages("ggplot2")
+}
+
+if (!is.installed("maps")){
+  install.packages("maps")
+}
+
+if (!is.installed("mapdata")){
+  install.packages("mapdata")
+}
+
+# Source code - https://stackoverflow.com/questions/9341635/check-for-installed-packages-before-running-install-packages
