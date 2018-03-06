9ef08e8c2043b11e8096e2e408ea6a7710eb6623
David Joslin <joslindr@gmail.com>
2018-03-05 21:24:52 -0800

    Stashed Changes

--- maps.R
+++ maps.R
@@ -1,10 +1,10 @@
-
 ##GGplot2 and dplyr must be installed on computer/drive in order for this script to function correctly
 
-
-library(ggplot2)
+source("packages.R")
 library(dplyr)
+library(ggplot2)
 library(maps)
+library(mapdata)
 
 world <- map_data("world")
 world <- world %>% filter(region != "Antarctica")
@@ -12,7 +12,7 @@ world$region <- tolower(world$region)
 
 CreateMap <- function(country){
   if(tolower(country) == "world"){
-    Area <- world 
+    Area <- world
   } else if (any(tolower(country) == world$region)){
     Area <- world %>% filter(region == tolower(country))
   } else{
@@ -21,5 +21,15 @@ CreateMap <- function(country){
   smallest <- min(Area$long)
   biggest <- max(Area$long)
   return(Area %>% ggplot(aes(long, lat)) + geom_polygon(aes(group = group)) + 
-           coord_cartesian(xlim = c(smallest, biggest)) + labs(title = paste(country, "Map"), x = "Longitude", y = "Latitude"))
+           coord_cartesian(xlim = c(smallest, biggest)) + labs(x = "Longitude", y = "Latitude"))
 }
+
+CreateMapHiRes <- function(country){
+  Area <- map_data(tolower(country)) # we already did this, but we can do it again
+  return(ggplot() + geom_polygon(data = Area, aes(x=long, y = lat, group = group)) + 
+           coord_fixed(1.3) + labs(x = "Longitude", y = "Latitude"))
+}
+
+#  return(Area %>% ggplot(aes(long, lat)) + geom_polygon(aes(group = group, fill = subregion)) + 
+#           coord_cartesian(xlim = c(smallest, biggest)) + labs(title = paste(country, "Map"), x = "Longitude", y = "Latitude"))
+
