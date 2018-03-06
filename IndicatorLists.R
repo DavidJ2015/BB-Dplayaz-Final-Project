547e794d5ad0f5f5b4548c0c938e409f18cd2bfc
Phi-Ho Phan <phihophan@gmail.com>
2018-03-06 01:56:29 -0800

    Added IndicatorList R file

---
+++ IndicatorLists.R
@@ -0,0 +1,18 @@
+Unemployment <- list(Unemployment_Percent.Tot.Labor.Force = "SL.UEM.TOTL.ZS", Unemployed.Males_Percent.Male.LF = "SL.UEM.TOTL.MA.ZS", 
+                     Unemployed_Percent.Female.LF = "SL.UEM.TOTL.FE.ZS", Percent.Youth.Not.In.Education.Employment.Or.Training = "SL.UEM.NEET.ZS",
+                     Long.Term.Unemployment_Percent.Tot.Unemp = "SL.UEM.LTRM.ZS", Percent.Unemployed.With.Basic.Ed = "SL.UEM.BASC.ZS", 
+                     Percent.Unemployed.With.Adv.Education = "SL.UEM.ADVN.ZS", Unemployment.To.Pop.Percent.Age.15.Plus = "SL.EMP.TOTL.SP.NE.ZS",
+                     Number.People.Employed = "L.EMP.TOTL", Tot.Employment.Male.Age.15.Plus = "SL.EMP.TOTL.MA", 
+                     Tot.Employment.Female.Age.15.Plus = "SL.EMP.TOTL.FE", Num.People.In.LF = "SL.TLF")
+
+Education <- list(Public.Expend.Education_Percent.GDP = "SE.XPD.EDUC.ZS", Percentage.Male.Repeat.Secondary.Ed = "UIS.REPP.2.GPV.M", 
+                  Percent.Repetition.Primary.Ed = "UIS.REPR.1", Illitereate.Pop.Age.15to24 = "UIS.LP.AG15T24", 
+                  Illiterate.Pop.Age.15.Plus = "UIS.LP.AG15T99", Percent.Literacy.Age.65.Plus = "UIS.LR.AG65",
+                  Primary.Enrollment.Percentage = "UIS.OAEP.1", Primary.Ed.Enrollment.Public = "UIS.E.1.Pu",
+                  Primary.Ed.Enrollment.Private = "UIS.E.1.PR", Percent.Literacy.Age.15.Plus ="SE.ADT.LITR.ZS",
+                  Percent.Male.Literacy.Age.15.Plus = "SE.ADT.LITR.MA.ZS", Percent.Female.Literacy.Age.15.Plus = "SE.ADT.LITR.FE.ZS",
+                  Tertiary.Pupil.Teacher.Ratio = "UIS.PTRHC.56", Upper.Secondary.Pupil.Teacher.Ratio = "UIS.PTRHC.3",
+                  Lower.Secondary.Pupil.Teacher.Ratio = "UIS.PTRHC.2", Percent.Children.Out.Of.School.Primary.School.Age = "SE.PRM.UNER.ZS",
+                  Educational.Attainment.Masters.Percent.Pop = "SE.TER.CUAT.MS.ZS", Educational.Attainment.Bachelors.Percent.Pop = "SE.TER.CUAT.BA.ZS",
+                  Educational.Attainment.Bachlors.Male = "SE.TER.CUAT.BA.MA.ZS", Educational.Attainment.Bachlors.Female = "SE.TER.CUAT.BA.FE.ZS",
+                  Educational.Attainment.Doctoral.Percent.Pop = "SE.TER.CUAT.DO.ZS")
