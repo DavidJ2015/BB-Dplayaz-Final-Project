40a4385ee65afbcc6dea2df9c9a87cd9dfc4af8d
Phi-Ho Phan <phihophan@gmail.com>
2018-03-06 04:04:54 -0800

    Added several lists of indicators

--- IndicatorLists.R
+++ IndicatorLists.R
@@ -16,3 +16,49 @@ Education <- list(Public.Expend.Education_Percent.GDP = "SE.XPD.EDUC.ZS", Percen
                   Educational.Attainment.Masters.Percent.Pop = "SE.TER.CUAT.MS.ZS", Educational.Attainment.Bachelors.Percent.Pop = "SE.TER.CUAT.BA.ZS",
                   Educational.Attainment.Bachlors.Male = "SE.TER.CUAT.BA.MA.ZS", Educational.Attainment.Bachlors.Female = "SE.TER.CUAT.BA.FE.ZS",
                   Educational.Attainment.Doctoral.Percent.Pop = "SE.TER.CUAT.DO.ZS")
+
+Population <- list(Population.Total = "P.POP.TOTL", Population.Male = "SP.POP.TOTL.MA.IN", Population.Female = "SP.POP.TOTL.FE.IN",
+                   Urban.Population = "SP.URB.TOTL", Rural.Population = "SP.RUR.TOTL", Percent.Urban.Population = "SP.URB.TOTL.IN.ZS",
+                   Percent.Rural.Population = "SP.RUR.TOTL.ZS", Population.Age.15to64 = "SP.POP.1564.TO", Percent.Population.Age.15to64 = "SP.POP.1564.TO.ZS",
+                   Percent.Male.Pop.Age.15to64 = "SP.POP.1564.MA.ZS", Percent.Female.Pop.Age.15to64 = "SP.POP.1564.FE.ZS", 
+                   Population.65.And.Up.2005and2010 = "SP.POP.65UP.TO", Birth.Rate.Crude.Per.1000 = "SP.DYN.CBRT.IN", 
+                   Death.Rate.Crude.Per.1000 = "SP.DYN.CDRT.IN", Infant.Mortality.Rate.Per.1000.Live = "SP.DYN.IMRT.IN", 
+                   Life.Exectancy.At.Birth = "SP.DYN.LE00.IN", Suicide.Mortality.Rate.Per.100000 = "SH.STA.SUIC.P5", Foreign.Labor.Force = "SM.TLF.FRGN.ZS", 
+                   Net.Migration = "SM.POP.NETM", Foreign.Population = "SM.POP.FRGN.ZS")
+
+Health <- list(Public.Health.Expenditure.Percent.GDP = "SH.XPD.PUBL.ZS", Health.Care.Percent.Pop.With.Access = "SH.STA.ACCH.ZS", 
+               Population.Per.Nurse = "SH.MED.NURS.ZS", Physicians.Per.1000 = "SH.MED.PHYS.ZS", Specialist.Surgical.Workforce.Per.100000 = "SH.MED.SAOP.P5",
+               Number.Of.Hospitals = "SH.HOSP.TOTL", Number.Of.Doctors = "SH.DR.TOTL", Percent.Pop.With.Basic.Sanitation.Dervices = "SH.STA.BASS.ZS",
+               Percent.Pop.With.Improved.Sanitation.Facilities = "SH.STA.ACSN", Percent.Rural.Pop.With.Improved.Sanitation.Services = "SH.STA.ACSN.RU",
+               Percent.Urban.Pop.With.Improved.Sanitation.Services = "SH.STA.ACSN.UR", Percent.Pop.With.Improved.Water.Source = "SH.H2O.SAFE.ZS",
+               Percent.Households.With.Water.On.Premise = "SG.H2O.PRMS.HH.ZS", Percent.Households.With.Water.30mins.Or.Longer.Away = "SG.H2O.TM30.HH.ZS",
+               Percent.Pop.HIV.Prevalence.Age.15to49 = "SH.DYN.AIDS.ZS", Adults.With.HIV.Age.15.Plus = "SH.DYN.AIDS", 
+               Total.People.With.HIV.And.AIDS = "SH.HIV.TOTL.NUM", Estimated.AIDS.Deaths = "SH.DYN.AIDS.DH", 
+               Percent.Mortality.From.CVD.Cancer.Diabetes.Or.CRD.Age.30to70 = "SH.DYN.NCOM.ZS", Percent.Prevalence.Overweight.Adults = "SH.STA.OWAD.ZS",
+               Percent.Prevalence.Overweight.Adult.Males = "SH.STA.OWAD.MA.ZS", Percent.Prevalence.Overweight.Adult.Females = "SH.STA.OWAD.FE.ZS",
+               Prevalence.Of.Undernourished = "SN.ITK.DEFC")
+
+Economy <- list(Total.Wealth.Constant.2014.US.Dollars = "NW.TOW.TO", Annual.Percentage.GDP.Growth = "NY.GDP.MKTP.KN.87.ZG", 
+                Gross.Domestic.Savings.Percentage.Of.GDP = "NY.GDS.TOTL.ZS", Annual.Percentage.Inflation = "NY.GDP.DEFL.KD.ZG",
+                Adjusted.Net.National.Income.2010.Constant.US.Dollars = "NY.ADJ.NNTY.KD",
+                Adjusted.Net.National.Income.Annual.Percent.Growth = "NY.ADJ.NNTY.KD.ZG", Human.Capital.2014.Constant.US.Dollars = "NW.HCA.TO",
+                Natural.Capital.Subsoil.Assets.2014.Constant.US.Dollars = "NW.NCA.SSOI.TO", 
+                Natural.Capital.Subsoil.Assets.Oil.2014.Constant.US.Dollars = "NW.NCA.SAOI.TO",
+                Natural.Capital.Subsoil.Assets.Silver.2014.Constant.US.Dollars = "NW.NCA.MSIL.TO",
+                Natural.Capital.Subsoil.Assets.Gold.2014.Constant.US.Dollars = "NW.NCA.MGOL.TO",
+                Natural.Capital.Agricultural.Land.Cropland.2014.Constant.US.Dollars = "NW.NCA.CROL.TO",
+                Natural.Capital.Forests.2014.Constant.US.Dollars = "NW.NCA.FORE.TO", Percent.Merchandise.Exports.Food = "TX.VAL.FOOD.ZS.UN",
+                Percent.Merchandise.Exports.Ores.And.Metals = "TX.VAL.MMTL.ZS.UN", Percent.Merchandise.Exports.Minerals = "TX.VAL.MNRL.UN.ZS",
+                Percent.Manufactured.Exports.High.Technology = "TX.VAL.TECH.MF.ZS", Net.Taxes.On.Products.Current.US.Dollars = "NY.TAX.NIND.CD",
+                Indirect.Taxes.Current.US.Dollars = "NY.TAX.IDRT.CD", International.Tourism.Expenditures.Current.US.Dollars = "ST.INT.XPND.CD",
+                Military.Expenditure.Percent.of.GDP = "MS.MIL.XPND.GD.ZS", Income.Share.Held.By.Highest.10.Percent = "SI.DST.10TH.10",
+                Income.Share.Held.By.Lowest.20.Percent = "SI.DST.FRST.20")
+
+FunFacts <- list(Smoking.Prevalence.Males.Percent.Adults = "SH.PRV.SMOK.MA", Smoking.Prevalence.Females.Percent.Adults = "SH.PRV.SMOK.FE",
+                 Aircraft.Departures.1000s = "SF.TRN.AIR.DPRT", Television.Sets.Per.1000.People = "SF.CMN.TELE.TH",
+                 Mobile.Phones.Per.1000.People = "SF.CMN.PHON.CELL.TH", Prencence.Of.Peace.Keepers.In.Mandate = "VC.PKP.TOTL.UN", 
+                 Intentional.Homicides.Per.100000.People = "VC.IHR.PSRC.P5", Intentional.Homicide.Rate.Per.100000.People = "VC.HOM.ITEN.P5.LE",
+                 Percent.Population.Age.15.Plus.With.Credit.Card = "WP_time_03.1", Percent.Population.With.Access.To.Electricity = "EG.ELC.ACCS.ZS")
+
+
+
