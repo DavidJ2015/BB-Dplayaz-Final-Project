c54cb4426705d839d52fa8aa429f7c3964b3eb45
Alejandro Huante <Alejandro@D-108-179-183-217.dhcp4.washington.edu>
2018-03-07 03:26:15 -0800

    Alejandro Charts

---
+++ charts.R
@@ -0,0 +1,43 @@
+library(ggplot2)
+library(dplyr)
+
+source("./API.R")
+
+#file_indicator <- 
+
+#variable <- staFiltered()[[2]]
+
+#columns, bar, pie
+#map_data <- read.table(...)
+
+list_of_countries <- list("Australia", "Brazil", "Canada", "China", "Mexico", "United States")
+
+#plots the bar graph
+Graph_Bar <- function(indicator, year) {
+  graph_filtered <- OneYear(year, list_of_countries, list(indicator))
+  p_bar_chart <- ggplot(graph_filtered, aes(name ,value, col = name)) + geom_bar(stat = "identity") +
+    coord_cartesian(ylim = c(0, 100)) +
+    coord_flip()
+  return(p_bar_chart)
+}
+
+# #Plots the dot graph with a smooth graph incorporated within
+# Graph_Dot <- function(indicatorOne, indicatorTwo, years, country) {
+#   indicatorOneUpdated <- list(indicatorOne, indicatorTwo)
+#   country_filter <- staByCountry(indicatorOneUpdated, years, country)
+#   forceOne <- as.numeric(country_filter$value)
+#   forceTwo <- as.numeric(country_filter$Value2)
+#    p_dot_chart <- ggplot(country_filter,  aes(forceOne, forceTwo)) + geom_point()
+# 
+#   return(p_dot_chart)
+# }
+
+#Pie Chart
+Graph_Pie <- function(indicator, year) {
+  graph_filtered <- OneYear(year, list_of_countries, list(indicator))
+  p_bar_chart <- ggplot(graph_filtered, aes(name ,value, col = name)) + geom_bar(stat = "identity") +
+    coord_cartesian(ylim = c(0, 100)) +
+    coord_flip()
+  pie <- p_bar_chart + coord_polar("y", start = 0) 
+  return(pie)
+}
